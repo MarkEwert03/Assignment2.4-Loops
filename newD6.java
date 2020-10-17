46b1ae16ff6e1b23bf2696a824c5dd54b0b526d5
Mark Ewert <mewert03@gmail.com>
2020-10-16 17:00:18 -0700

    Fixed bug in newD6

--- newD6.java
+++ newD6.java
@@ -25,7 +25,10 @@ public class newD6{
             System.out.print("Again? [y, n] ");
             String response = sc.next();
             if ("y".equals(response)) System.out.println("");
-            else if ("y".equals(response))continuing = false;
+            else if ("n".equals(response)){
+                System.out.println("Goodbye!");
+                continuing = false;
+            }
             else {
                 System.out.println("That is not a valid response. Terminating game now");
                 continuing = false;
